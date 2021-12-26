361d76a750c4210639a39adc8eb473591b6b9c5e
Torrel <taurel107@gmail.com>
2021-12-26 10:49:50 -0500

    added hardcoded playlist to the playlist component

--- client/Components/App/App.js
+++ client/Components/App/App.js
@@ -10,9 +10,15 @@ class App extends React.Component {
         super(props)
           this.state = {
               searchResults: [
-                {name: 'name1', artist: 'artist1', album: 'album1', id: 'id1'},
-                {name: 'name2', artist: 'artist2', album: 'album2', id: 'id2'},
-                {name: 'name3', artist: 'artist3', album: 'album3', id: 'id3'}
+                {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+                {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+                {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+              ],
+              playlistName: "playlistName1",
+              playlistTracks: [
+                  {name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4},
+                  {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5},
+                  {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}
               ]
           }
    }  
@@ -26,7 +32,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
           <SearchResults searchResults={this.state.searchResults} /> 
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
